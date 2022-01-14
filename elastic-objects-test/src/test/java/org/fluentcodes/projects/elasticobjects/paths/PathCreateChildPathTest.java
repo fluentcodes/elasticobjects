042c7d2f4a5269bc4d2fd2731c8e61d4539763cb
Diwischek <fluentcodes@gmail.com>
2022-01-14 14:26:28 +0100

    V0.9.3 cleanup Path

--- elastic-objects-test/src/test/java/org/fluentcodes/projects/elasticobjects/paths/PathCreateChildPathTest.java
+++ elastic-objects-test/src/test/java/org/fluentcodes/projects/elasticobjects/paths/PathCreateChildPathTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import static org.fluentcodes.projects.elasticobjects.EoTestStatic.S_LEVEL0;
 import static org.fluentcodes.projects.elasticobjects.EoTestStatic.S_LEVEL1;
 import static org.fluentcodes.projects.elasticobjects.EoTestStatic.S_LEVEL2;
+import static org.fluentcodes.projects.elasticobjects.Path.DELIMITER;
+import static org.fluentcodes.projects.elasticobjects.PathElement.SAME;
 
 public class PathCreateChildPathTest {
 
@@ -15,22 +17,22 @@ public class PathCreateChildPathTest {
     public void given3Elements_then2Elements() {
         Path path = new Path(S_LEVEL0, S_LEVEL1, S_LEVEL2);
         Path childPath = path.createChildPath();
-        Assertions.assertThat(childPath.directory()).isEqualTo(Path.ofs(S_LEVEL1, S_LEVEL2));
+        Assertions.assertThat(childPath.directory()).isEqualTo(
+                String.join(DELIMITER, new String[]{S_LEVEL1, S_LEVEL2}));
     }
 
     @Test
     public void given1Element_then0Elements() {
         Path path = new Path(S_LEVEL0);
         Path childPath = path.createChildPath();
-        Assertions.assertThat(childPath.directory()).isEmpty();
-        ;
+        Assertions.assertThat(childPath.directory()).isEqualTo(SAME);;
     }
 
     @Test
     public void given1ElementAbsolute_then0Elements() {
-        Path path = new Path(Path.DELIMITER, S_LEVEL0);
+        Path path = new Path(DELIMITER, S_LEVEL0);
         Path childPath = path.createChildPath();
-        Assertions.assertThat(childPath.directory()).isEqualTo(Path.DELIMITER);
+        Assertions.assertThat(childPath.directory()).isEqualTo(DELIMITER);
     }
 
 }
